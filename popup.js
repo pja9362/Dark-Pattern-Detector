9eb0d14d69f5ab737ead37f67fc8763cc60feec5
jina <pja9362@g.skku.edu>
2024-05-31 21:13:51 +0900

    feat(scrape): 스크래핑 및 다크패턴 교정 코드 추가 최종본

--- popup.js
+++ popup.js
@@ -1,7 +1,23 @@
 // popup.js
-document.getElementById('apply-gpt').addEventListener('click', function() {
+// document.getElementById('apply-gpt').addEventListener('click', function() {
+//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//         const activeTab = tabs[0];
+//         chrome.tabs.sendMessage(activeTab.id, { action: "apply-gpt" });
+//     });
+// });
+
+document.getElementById('detect-price-pattern').addEventListener('click', function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         const activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, { action: "apply-gpt" });
+        chrome.tabs.sendMessage(activeTab.id, { action: "detect-price-pattern" });
+
+        const status = document.getElementById('loading-status');
+        status.innerText = 'Loading...';;
+        
+        chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+            if (message.success) {
+                status.innerText = '';
+            }
+        });
     });
 });
